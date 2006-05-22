de7eca804e510334d6d736070e61cb4ddf4e1a22
Gary King <gwking@metabang.com>
2006-05-22 16:55:12 -0400

    Package : / #: cleanup ... more!
    darcs-hash:20060522205512-3cc5d-6bc2a51b8ce9c7c1af28fcf12076a4a448ec2563.gz

--- dev/utilities/package-additional.lisp
+++ dev/utilities/package-additional.lisp
@@ -4,15 +4,15 @@
 (export-exported-symbols 'metabang.bind 'metatilities)
 
 (eval-when (:compile-toplevel :load-toplevel)
-  (shadowing-import '(copy-file) 'cl-fad)
+  (shadowing-import '(#:copy-file) '#:cl-fad)
 
   ;;?? Gary King 2005-07-12: not quite sure about this one.
-  (shadowing-import '(containers:root) 'metatilities)
-  (shadowing-import '(containers:move) 'metatilities)
+  (shadowing-import '(containers:root) '#:metatilities)
+  (shadowing-import '(containers:move) '#:metatilities)
 
-  (use-package 'cl-fad 'metatilities))
+  (use-package '#:cl-fad '#:metatilities))
 
-(export-exported-symbols 'containers 'metatilities)
+(export-exported-symbols '#:containers '#:metatilities)
 
 (make-load-form* containers::bst-node)
 (make-load-form* containers::quad-tree-node)
