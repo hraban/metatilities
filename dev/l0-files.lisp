903090930acaefcf0a7df7c7189cc6bc251c3947
Gary King <gwking@metabang.com>
2006-06-02 12:51:29 -0400

    Moved physical-pathname-directory-separator from files to l0-files (i.e., from metatilities to metatilities-base)
    darcs-hash:20060602165129-3cc5d-0e98cb4c35d4c0e4ca031b19004a3341d3970617.gz

--- dev/l0-files.lisp
+++ dev/l0-files.lisp
@@ -24,3 +24,18 @@
   ()
   (:test ((ensure-same (macintosh-path->unix "users:gwking:test-db.db")
                        "/users/gwking/test-db.db" :test string-equal))))
+
+(defun physical-pathname-directory-separator ()
+  "Returns a string representing the separator used to delimit directories in a physical pathname. For example, Digitool's MCL would return \":\" whereas OpenMCL would return \"/\"."
+  (let* ((directory-1 "foo")
+         (directory-2 "bar")
+         (pn (namestring
+              (translate-logical-pathname
+               (make-pathname
+                :host nil
+                :directory `(:absolute ,directory-1 ,directory-2)
+                :name nil
+                :type nil))))
+         (foo-pos (search directory-1 pn :test #'char-equal))
+         (bar-pos (search directory-2 pn :test #'char-equal)))
+    (subseq pn (+ foo-pos (length directory-1)) bar-pos)))
--- dev/utilities/files.lisp
+++ dev/utilities/files.lisp
@@ -640,25 +640,7 @@ the object file."
   (touch-file "user-home:temporary;foo;bar;biz;what.y")
   (touch-file "user-home:temporary;foo;bar;biz;what.z"))
 #+Test
-(delete-directory "user-home:temporary;foo;" :verbose? t :dry-run? t)
-
-;;; ---------------------------------------------------------------------------
-
-(defun physical-pathname-directory-separator ()
-  "Returns a string representing the separator used to delimit directories in a physical pathname. For example, Digitool's MCL would return \":\" whereas OpenMCL would return \"/\"."
-  (let* ((directory-1 "foo")
-         (directory-2 "bar")
-         (pn (namestring
-              (translate-logical-pathname
-               (make-pathname
-                :host nil
-                :directory `(:absolute ,directory-1 ,directory-2)
-                :name nil
-                :type nil))))
-         (foo-pos (search directory-1 pn :test #'char-equal))
-         (bar-pos (search directory-2 pn :test #'char-equal)))
-    (subseq pn (+ foo-pos (length directory-1)) bar-pos)))
-        
+(delete-directory "user-home:temporary;foo;" :verbose? t :dry-run? t)        
 
 
 ;;; ***************************************************************************
