ad10ca55bb14d7081be5cae44090c174ba4d2760
Gary King <gwking@metabang.com>
2007-01-18 21:00:13 -0500

    Added test and key keyword arguments to set-equal
    darcs-hash:20070119020013-3cc5d-298b21455f42be6991cbfcca94b9a5f8f4c70140.gz

--- dev/set-equal.lisp
+++ dev/set-equal.lisp
@@ -1,17 +1,17 @@
 (in-package #:metatilities)
 
-(defun set-equal (list1 list2 &rest args)
+(defun set-equal (list1 list2 &rest args
+		  &key test key)
   "Returns t if list1 and list2 are equal (as sets). If list1 and list2 are not
 equal returns (as multiple values) nil and two lists. The first list contains the 
 elements in list1 and not in list2 and the second list contains elements in list2 
 and not in list1."
+  (declare (ignore test key))
   (let ((in1-not2 (apply #'set-difference list1 list2 args))
         (in2-not1 (apply #'set-difference list2 list1 args)))
     (if (apply #'union in1-not2 in2-not1 args)
       (values nil in1-not2 in2-not1)
       (values t nil nil))))
-
-;;; ---------------------------------------------------------------------------
 
 #+Alternate
 ;; some very quick testing doesn't show any real improvement
