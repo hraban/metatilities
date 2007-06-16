3c3eceb190cfea0f48b276c46f8228f815fbd7bd
Gary King <gwking@metabang.com>
2007-06-16 15:23:09 -0400

    futzing with leap-year-p testing
    darcs-hash:20070616192309-3cc5d-0d0190c6fd1344cde5753c149cb982def9805009.gz

--- dev/utilities/test-dates-and-times.lisp
+++ dev/utilities/test-dates-and-times.lisp
@@ -7,3 +7,19 @@
   (:test ((lift:ensure (leap-year-p 2000))))
   (:test ((lift:ensure (leap-year-p 1996))))
   (:test ((lift:ensure (not (leap-year-p 1997))))))
+
+#|
+(use-package 'lift)
+
+(deftestsuite test-leap-year-p ()
+  ()
+  (:tests
+   ((ensure (leap-year-p 1904)))
+   (div-by-four (ensure (leap-year-p 2000)))
+   ((ensure (leap-year-p 1996))))
+  (:test ((ensure-null (leap-year-p 1900))))
+  (:test ((ensure-null (leap-year-p 1997)))))
+
+(print-tests :start-at 'test-leap-year-p)
+
+|#
