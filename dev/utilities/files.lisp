e0ff4ac793fc34515c5dc8cee3f969041eaf4dd7
Gary King <gwking@metabang.com>
2008-03-01 14:16:36 -0500

    renamed the map-lines macro to iterate-lines
    darcs-hash:20080301191636-3cc5d-7d1540883a6f73fbb28b7b2f9bab5489773eec63.gz

--- dev/utilities/files.lisp
+++ dev/utilities/files.lisp
@@ -207,7 +207,7 @@ additional args are passed along to the function."
           until (eq l :eof) do
           (funcall fn l))))
 
-(defmacro map-lines (fn source)
+(defmacro iterate-lines (fn source)
   "An enpowered version of map-lines-in-file. This one is a macro
 so that you can use (return) to stop processing and so that you can
 use (next-line) within your code to advance to the next line of the 
