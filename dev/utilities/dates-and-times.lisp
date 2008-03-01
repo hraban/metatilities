00fc0fb6b6c7ad7be4468eac1c469f2cd7e6cf26
Gary King <gwking@metabang.com>
2008-03-01 14:14:50 -0500

    Pulled some stuff out of dates-and-times and into l0-time
    darcs-hash:20080301191450-3cc5d-8db403dc02acff30c67e6471a3213a981223758d.gz

--- dev/utilities/dates-and-times.lisp
+++ dev/utilities/dates-and-times.lisp
@@ -1,28 +1,8 @@
 (in-package #:metatilities)
 
-
 ;;; ---------------------------------------------------------------------------
 ;;; obvious constants
 ;;; ---------------------------------------------------------------------------
-
-(defconstant +minutes-per-hour+ 60
-  "The number of minutes in one hour.")
-
-(defconstant +seconds-per-minute+ 60
-  "The number of seconds in one minute.")
-
-(defconstant +usual-days-per-year+ 365
-  "The number of days in an ordinary year.")
-
-(defconstant +seconds-per-hour+ (* +seconds-per-minute+ +minutes-per-hour+)
-  "The number of seconds in one hour.")
-
-(defconstant +hours-per-day+ 24
-  "The number of hours in one day.")
-
-(defconstant +seconds-per-day+
-  (* +hours-per-day+ +seconds-per-hour+)
-  "The number of seconds in one day.")
 
 ;;; these are lists of acceptable tokens
 ;;; make sure to use 'index-of' on these; it allows you to find an index in a deep list
@@ -45,9 +25,6 @@
 (defparameter +dow-output-list
   '(("Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday" "Sunday")
     ("Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun")))
-
-(defparameter +days-per-month+
-  '(31 28 31 30 31 30 31 31 30 31 30 31))
 
 ;;; ---------------------------------------------------------------------------
 ;;; en/de-coding functions
@@ -742,31 +719,6 @@ if stream is nil, construct and return a string."
 		(t "am"))))
 
 ;;; ---------------------------------------------------------------------------
-
-(eval-always 
-  (defmacro generate-time-part-function (part-name position)
-    (let ((function-name (form-symbol (symbol-name 'time) "-" part-name)))
-      `(eval-always
-         (export ',function-name)
-         (defun ,function-name
-                (&optional (universal-time (get-universal-time))
-                           (time-zone nil))
-           ,(format nil "Returns the ~(~A~) part of the given time." part-name)
-           (nth-value ,position (apply #'decode-universal-time universal-time time-zone))))))
-
-  ;;; ---------------------------------------------------------------------------
-  
-  (generate-time-part-function second 0)
-  (generate-time-part-function minute 1)
-  (generate-time-part-function hour 2)
-  (generate-time-part-function date 3)
-  (generate-time-part-function month 4)
-  (generate-time-part-function year 5)
-  (generate-time-part-function day-of-week 6)
-  (generate-time-part-function daylight-savings-time-p 7))
-
-
-;;; ---------------------------------------------------------------------------
 ;;; format-date
 ;;; ---------------------------------------------------------------------------
 
@@ -890,30 +842,6 @@ None of %c, %F, %p, %x, %X, %Z, %z are implemented."
                              (error "Ouch - unknown formatter '%~c" char))))
                           (t char)))))))
   
-;;; ---------------------------------------------------------------------------
-
-(defun days-in-month (month &optional leap-year?)
-  "Returns the number of days in the specified month. The month should be
-between 1 and 12."
-  (+ (nth (1- month) +days-per-month+) (if (and (= month 2) leap-year?) 1 0)))
-
-;;; ---------------------------------------------------------------------------
-
-(defun leap-year-p (year)
-  "Returns t if the specified year is a leap year. I.e. if the year
-is divisible by four but not by 100 or if it is divisible by 400."
-  (or (and (= (mod year 4) 0)               ; logand is faster but less perspicuous
-           (not (= (mod year 100) 0)))
-      (= (mod year 400) 0)))
-
-;;; ---------------------------------------------------------------------------
-
-(defun day-of-year (date)
-  "Returns the day of the year [1 to 366] of the specified date [which must be \(CL\) universal time format.]" 
-  (let ((leap-year? (leap-year-p (time-year date))))
-    (+ (loop for month from 1 to (1- (time-month date)) sum
-             (days-in-month month leap-year?))
-       (time-date date))))
 
 ;;; ***************************************************************************
 ;;; *                              End of File                                *
