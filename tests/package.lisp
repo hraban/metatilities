2d0a4db507366f6af75c3ca33d37fd6575053833
Gary King <gwking@metabang.com>
2008-09-27 20:28:51 -0400

    some new tests for parse-time
    darcs-hash:20080928002851-3cc5d-5b0c7efb205e6a064d02c750e390e029bfb7c5b0.gz

--- tests/package.lisp
+++ tests/package.lisp
@@ -3,7 +3,20 @@
 (defpackage #:metatilities-test
   (:use #:common-lisp #:lift #:metatilities)
   (:import-from #:metatilities
+		#:time-year
+		#:time-month
+		#:time-date
+		#:time-hour
+		#:time-minute
+		#:time-second
+		#:day-of-week
+		#:time-daylight-savings-time-p)
+  (:import-from #:metatilities
                 #:*automatic-slot-accessors?*
                 #:*automatic-slot-initargs?*
-                #:*prune-unknown-slot-options*))
+                #:*prune-unknown-slot-options*)
+  (:import-from #:metatilities
+		#:parse-time
+		)
+  )
 
